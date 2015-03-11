d764151d3f2de339beae7d2d954fc3cc49381c6a
Alexandru Ungurianu <alexandru.ungurianu@gmail.com>
2015-03-11 21:24:37 +0200

    Added a Factorials class for precomputing factorials

--- Python/pemath.py
+++ Python/pemath.py
@@ -25,6 +25,28 @@ class PrimeNumbers:
 	def get_nth_prime(self,n):
 		return self.primes[n]
 
+class Factorials:
+	def __init__(self,up_to=None,n=None):	
+		if up_to != None:
+			self.factorials = [1]
+			x = 1
+			while(self.factorials[-1]*x <= up_to):
+				self.factorials.append(self.factorials[-1]*x)
+				x+=1
+		elif n != None:
+			self.factorials = [1]
+			for x in range(1,n+1):
+				self.factorials.append(self.factorials[-1]*x)
+		else:
+			raise ValueError	
+
+	def get_factorials(self):
+		return self.factorials
+	def get_nth_factorial(self,n):
+		return self.factorials[n]
+	def comb(self,n,k):
+		return self.factorials[n]/(self.factorials[k]*self.factorials[n-k])
+
 def gcd(a,b):
 	if b==0:
 		return a
