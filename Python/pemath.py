4ae2df61f82e9c13915b5dcfec7477e57ea4dd33
Alexandru Ungurianu <alexandru.ungurianu@gmail.com>
2015-03-11 20:47:29 +0200

    The beginnings of a math library

---
+++ Python/pemath.py
@@ -0,0 +1,22 @@
+import math
+
+class PrimeNumbers:
+	def __init__(self,N):
+		self.limit = N
+		self.precalc_primes(N)
+
+	def precalc_primes(self,N):
+		self.sieve = [True]*(N+1)
+		for d in range(2,int(N**0.5+1)):
+			if self.sieve[d]:
+				for i in range(2*d,N+1,d):
+					self.sieve[i] = False
+		self.primes = [i for i in range(2,N+1) if self.sieve[i]]
+
+	def is_prime(self,n):
+		if n > self.limit:
+			raise IndexError
+		return self.sieve[n]
+
+	def get_first_primes(self,n):
+		return self.primes[:n]
