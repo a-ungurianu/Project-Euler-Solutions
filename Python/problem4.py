4ffa350608ba522222c6acb28d16b4fe7e10cd78
Alexandru Ungurianu <alexandru.ungurianu@gmail.com>
2015-03-11 21:27:52 +0200

    Imported stuff from pemath

--- Python/problem4.py
+++ Python/problem4.py
@@ -1,18 +1,10 @@
-
-
-def isPalindrome(n):
-	s = str(n)
-	if len(s) % 2 != 0:
-		return False
-	else:
-		return s[:len(s)/2] == s[:len(s)/2-1:-1]
-
+from pemath import is_palindrome
 
 pals = []
 
 for i in range(100,1000):
 	for j in range(100,i+1):
-		if isPalindrome(i*j):
+		if is_palindrome(i*j):
 			pals.append(i*j)
 
 print(max(pals))
