d45a295108b2d63c7189ed9f425d603a8221b5b2
Alexandru Ungurianu <alexandru.ungurianu@gmail.com>
2015-03-11 21:25:04 +0200

    Rewritten to use the pemath module

--- Python/problem53.py
+++ Python/problem53.py
@@ -1,19 +1,13 @@
-fact = []
-f = 1
-for i in range(1,101):
-    fact.append(f)
-    f *= i
-fact.append(f)
+import pemath
 
-def comb(n,k):
-    return fact[n]/(fact[k]*fact[n-k])
+factorials = pemath.Factorials(n=102)
 
 LIMIT = 100
 
 count = 0
 for n in range(1,LIMIT+1):
     for r in range(1,n+1):
-        if comb(n,r) > 1000000:
+        if factorials.comb(n,r) > 1000000:
             count+=1
-
+            
 print(count)
