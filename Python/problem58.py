b20479d6f057e47b9e4616657480d9f7d9bcea49
Alex Ungurianu <alexandru.ungurianu@gmail.com>
2015-12-10 15:28:36 +0000

    Changed approach to checking if the numbers on the diagonals are prime [FAILED]
    The approach seems sound, and it actually gives a result instead of looping forever.
    I think the blame falls on floating point precision errors.

--- Python/problem58.py
+++ Python/problem58.py
@@ -1,14 +1,30 @@
-from math import ceil
 from itertools import count
+from pemath import is_prime
 
-p = lambda x:2*x + ceil(float(x)/2)
-t = lambda x:4*x + 1
+bottom_right = lambda x: (2*x+1)**2
+bottom_left = lambda x: (2*x+1)**2 - 2*x
+top_left = lambda x: (2*x+1)**2 - 4*x
+top_right = lambda x: (2*x+1)**2 - 6*x
 
+
+count_primes = 0
+count_total = 1
+
+#primes = PrimeNumbers(int(4e8))
 
 for x in count(1):
-    if p(x)/t(x) <= 1/10:
-        print(x)
-        break
+    count_total += 4
+    if(is_prime(bottom_left(x))):
+        count_primes += 1
+    if(is_prime(top_left(x))):
+        count_primes += 1
+    if(is_prime(top_right(x))):
+        count_primes += 1
 
+    ratio = float(count_primes)/count_total
+    if ratio*100 < 10:
+        print(x,ratio)
+        break
     if x % 100 == 0:
-        print(str(x) + " " + str(p(x)/t(x)))
+        print(x,ratio)
+
